b96d8f4bc5aacc10f3fc16b757ac658fc89dc0f6
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-09-10 04:29:13 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,6 +2,16 @@ def square_array(numbers)
   count = 0
   while count < numbers do
  numbers.each do |num|
-   p num ** 2
+   p count ** 2
  end
 end
+
+
+# def output_array_elements(array)
+#   counter = 0
+ 
+#   while array[counter] do
+#     puts array[counter]
+#     counter += 1
+#   end
+# end
